42a7ac442db8e3835ce2f4b1aba12fe19a70a88b
wangfan <wangfan@keanbang.com>
2024-08-02 17:35:01 +0800

    chore: 调整minio容器

--- minio/init.sh
+++ minio/init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # 设置 MinIO 别名
-mc alias set myminio http://minio:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+mc alias set myminio http://localhost:9000 $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
 
 # 创建存储桶
 mc mb myminio/aibutler
